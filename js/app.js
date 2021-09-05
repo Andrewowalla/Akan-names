ef3ce542faa91dbb840f7ca136b9e4a896936e0e
Andrewowalla <andrewowalla@gmail.com>
2021-09-06 00:39:43 +0300

    add constant

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const checkAkan = (e) => {
+    e.preventDefault();
+    const mNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+    const fNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+}
