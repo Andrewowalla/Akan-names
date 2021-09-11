5c5715277051ceea23f4a7701b23c3cb6aba2454
Andrewowalla <andrewowalla@gmail.com>
2021-09-11 10:46:49 +0300

    add fullstop

--- js/app.js
+++ js/app.js
@@ -1,24 +1,23 @@
-const checkAkan = (e) =>{
-  e.preventDefault();
-const mNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-const fNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-
-const frm=document.querySelector("frmAkans");
-const fd = new FormData(frmAkan);
-const gender=fd.get("gender");
-const birthday=fd.get("dateOfBorn");
-    
-const d =new Date(centuryInput, yearInput, (monthInput-1), dateInput).getDay();
-
-if(dateOfBorn==="") {
-  alert("Invalid Input");
-}
-
-if (gender === "male"){
-  document.querySelector ("#Return").innerHTML=("Your Akan name is "+mNames[d]);
-} else{
-  document.querySelector ("#Return").innerHTML=("Your Akan name is "+fNames[d]);
-}
-}
-
-document.querySelector("#frmAkan").addEventListener("submit", checkAkan);
+const giveAkanname = function (e) {
+    e.preventDefault();
+    const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama",];
+    const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  
+    const dayOfBirth = document.getElementById("dateOfBirth").value;
+    if (!dayOfBirth) {
+      return alert("Please Select Birthday");
+    }
+    const day = new Date(dayOfBirth).getDay();
+    const femaleGender = document.getElementById("gender").checked;
+    const maleGender = document.getElementById("gender").checked;
+    if (femaleGender) {
+        document.querySelector('#reveal-result').innerHTML = "Your Akan name is " + femaleNames[day];
+    } else if (maleGender) {
+        document.querySelector('#reveal-result').innerHTML = "Your Akan name is " + maleNames[day];
+    } else {
+      return alert("Please select Gender");
+    }
+    document.getElementById("frmAkans").reset();
+  };
+  
+  document.querySelector("#frmAkans").addEventListener("submit", giveAkanname);
